16388e277a011749e9e4c8bd76985bbbdec4249a
Darkhan Kubigenov <darkhanu@gmail.com>
2022-12-06 08:31:42 +0000

    day 6

--- src/day6/main.rs
+++ src/day6/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use advent_2022::utils::read_lines;
 
 fn main() {
@@ -6,20 +8,32 @@ fn main() {
 }
 
 fn part_one() {
-    if let Ok(lines) = read_lines("./inputs/day3.txt") {
+    if let Ok(lines) = read_lines("./inputs/day6.txt") {
         for line in lines {
             if let Ok(ip) = line {
-                println!("Line is {}", ip)
+                for idx in 0..ip.len() - 4 {
+                    let x: HashSet<char> = HashSet::from_iter(ip[idx..idx + 4].chars());
+                    if x.len() == 4 {
+                        println!("[part one] Found start index {}", idx + 4);
+                        return;
+                    }
+                }
             }
         }
     }
 }
 
 fn part_two() {
-    if let Ok(lines) = read_lines("./inputs/day3.txt") {
+    if let Ok(lines) = read_lines("./inputs/day6.txt") {
         for line in lines {
             if let Ok(ip) = line {
-                println!("Line is {}", ip)
+                for idx in 0..ip.len() - 14 {
+                    let x: HashSet<char> = HashSet::from_iter(ip[idx..idx + 14].chars());
+                    if x.len() == 14 {
+                        println!("[part two] Found start index {}", idx + 14);
+                        return;
+                    }
+                }
             }
         }
     }
